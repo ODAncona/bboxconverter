ddbd2e9452e2673e4ef3b39124c1225849574139
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2023-03-15 08:36:49 +0100

    test train_test_split

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,24 +1,26 @@
 # Changelog
 
+## v0.1.8 (2023-03-x)
+    - Write some tests
 
 ## v0.1.7 (2023-03-14)
 
     - Added read_the_docs documentation
-    - Added CI, CD
+    - Added CI,CD
 
 ## v0.1.6 (2023-03-14)
 
     - Added read_coco
-    - added to_coco
+    - Added to_coco
     - Added read_manifest
 
 ## v0.1.5 (2023-03-13)
 
     - Added tests
     - Added badges
-    - added read_csv
-    - added to_csv
-    - 
+    - Added read_csv
+    - Added to_csv
+ 
 ## v0.1.2 (2023-03-05)
 
     - Initial release
--- src/tests/test_train_test_split.py
+++ src/tests/test_train_test_split.py
@@ -1,8 +1,59 @@
-class TestClass():
-    def train_test_split_dont_mix_bbox_between_files(self):
-        assert True
+import pytest
+import bboxconverter as bc
+from pathlib import Path
+import json
+import pandas as pd
+import shutil
 
-    def train_test_split_moved_all_img_successfully(self):
-        assert True
-    
-    
+@pytest.fixture
+def export_csv_to_coco():
+    input_path = Path("src/tests/test_data/cat_dog.csv")
+    output_path = Path("src/tests/test_data/cat_dog.json")
+    bbox_map = dict(
+        file_path='file_name',
+        class_name='rectanglelabels',
+        x_min='x',
+        y_min='y',
+        width='width',
+        height='height',
+        image_width='original_width',
+        image_height='original_height',
+    )
+    parser = bc.read_csv(input_path, mapping=bbox_map)
+    parser.export(output_path, format="coco")
+    yield output_path, parser
+    output_path.unlink()
+
+
+@pytest.fixture
+def train_test_split(export_csv_to_coco):
+    original_path, parser = export_csv_to_coco
+    parser.export(original_path, format="coco", split=True)
+    file_name = original_path.name
+    train_path = Path(original_path.parent / "train" / file_name)
+    test_path = Path(original_path.parent / "test" / file_name)
+    yield file_name, train_path, test_path, original_path, parser
+    shutil.rmtree(train_path.parent)
+    shutil.rmtree(test_path.parent)
+
+
+class TestClass():
+    def test_train_test_split_no_data_lost(self, train_test_split):
+        file_name, train_path, test_path, original_path, parser = train_test_split
+        train_annotation = train_path.parent / file_name
+        test_annotation = test_path.parent / file_name
+        parser_train = bc.read_coco(train_annotation)
+        parser_test = bc.read_coco(test_annotation)
+        assert len(parser.data) == len(parser_train.data) + len(parser_test.data)
+
+    def test_train_test_split_no_annotations_lost(self, train_test_split):
+        file_name, train_path, test_path, original_path, parser = train_test_split
+        train_annotation = train_path.parent / file_name
+        test_annotation = test_path.parent / file_name
+        parser_train = bc.read_coco(train_annotation)
+        parser_test = bc.read_coco(test_annotation)
+        cnt = parser.data['file_path'].value_counts()
+        cnt_train = parser_train.data['file_path'].value_counts()
+        cnt_test = parser_test.data['file_path'].value_counts()
+        assert (pd.concat([cnt_train, cnt_test], axis=0) == cnt).all()
+
